91ef0c41f718438c54ed1cdb38b8fb36bd7e8e8c
yjy5264 <yjy5264@sina.com>
2020-07-24 14:19:12 +0800

    1

---
+++ 322.零钱兑换_1.ts
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=322 lang=typescript
+ *
+ * [322] 零钱兑换
+ */
+
+// @lc code=start
+function coinChange_1(coins: number[], amount: number): number {
+    interface Node {
+        value: number;
+        depth: number;
+        next?: Node[];
+    }
+
+    let numbers: number[] = [];
+
+    const getNext = (parent: Node) => {
+        if (parent.value === amount) numbers.push(parent.depth);
+        if (parent.value >= amount) return undefined;
+        return coins.map(item => ({ value: parent.value + item, depth: parent.depth + 1}));
+    }
+    
+    const root: Node = { value: 0, depth: 0 };
+    root.next = getNext(root);
+    const fn = (next?: Node[]) => {
+        next && next.map(node => {
+            if (node) {
+                node.next = getNext(node);
+                node.next && fn(node.next);
+            }
+        });
+    }
+    fn(root.next);
+
+    if (numbers.length === 0) return -1;
+    return Math.min(...numbers);
+};
+// @lc code=end
+
